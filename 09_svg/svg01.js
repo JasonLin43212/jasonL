89adf44ec4113abba03924fd9eeaafd9603ef5f8
Jason Lin <jasonlin43212@gmail.com>
2019-03-13 09:42:09 -0400

    updated work

--- 09_svg/svg01.js
+++ 09_svg/svg01.js
@@ -7,59 +7,51 @@ K09 -- Connect the Dots
 
 var pic = document.getElementById("vimage");
 var clearButton = document.getElementById("but_clear");
+
+var isClear = true; // Tells the program if there is no dots on screen
 var prevX = null;
 var prevY = null;
 
-var drawDot = function(x,y) {
-  var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  c.setAttribute( "cx", x );
-  c.setAttribute( "cy", y );
-  c.setAttribute( "r", "5" );
-  c.setAttribute( "fill", "#B497D6" );
-  c.setAttribute( "stroke", "black" );
-  pic.appendChild( c );
-}
+var draw_shapes = function(x,y) {
 
-var drawLine = function(x0, y0, x1, y1) {
-  var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  l.setAttribute('x1', x0);
-  l.setAttribute('y1', y0);
-  l.setAttribute('x2', x1);
-  l.setAttribute('y2', y1);
-  l.setAttribute('stroke', "#AEC6CF");
-  pic.appendChild( l );
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute( "cx", x );
+    c.setAttribute( "cy", y );
+    c.setAttribute( "r", "10" );
+    c.setAttribute( "fill", "#ff0000" );
+    pic.appendChild( c );
+
+    if (!isClear){
+
+        var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        l.setAttribute('x1', x);
+        l.setAttribute('y1', y);
+        l.setAttribute('x2', prevX);
+        l.setAttribute('y2', prevY);
+        l.setAttribute('stroke', "#000000");
+        pic.appendChild( l );
+    }
+    prevX = x;
+    prevY = y;
+    isClear = false;//Tells the program that the user has draw first dot
 }
 
 var clear = function() {
-  prevX = null;
-  prevY = null;
-  while (pic.firstChild) { //Deletes all children
-    pic.removeChild(pic.firstChild);
-  }
+    prevX = null;
+    prevY = null;
+    isClear = true;
+    while (pic.firstChild) { // Removes all children while there are children
+        pic.removeChild(pic.firstChild);
+    }
 }
 
-var draw_dot = e => {
+var draw = e => {
     event.preventDefault(); //Stops click action from being executed
     var x = event.offsetX; //X cor of mouse
     var y = event.offsetY; //Y cor of mouse
 
-    if (prevX != null) {
-      var vector = [x - prevX, y - prevY]; //Vector created from the previous point to the new point
-      var mag = Math.sqrt(Math.pow(vector[0],2) + Math.pow(vector[1],2)) //Magnitude of the vector
-      //Normalize vector
-      vector[0] /= mag; //
-      vector[1] /= mag; //
-      //Multiply vector by circle radius
-      vector[0] *= 5; //
-      vector[1] *= 5; //
-      x0 = prevX + vector[0]; //Offset x and y by vector
-      y0 = prevY + vector[1];
-      drawLine(x0, y0, x, y);
-    }
-    drawDot(x,y);
-    prevX = x;
-    prevY = y;
+    draw_shapes(x,y);
 }
 
 clearButton.addEventListener('click', clear);
-pic.addEventListener('click', draw_dot);
+pic.addEventListener('click', draw);
