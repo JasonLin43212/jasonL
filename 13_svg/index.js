4463d0d6677d34bcc162f319687bf1d271e86275
Jason Lin <jasonlin43212@gmail.com>
2019-03-20 12:23:29 -0400

    made chart appear

--- 13_svg/index.js
+++ 13_svg/index.js
@@ -7,7 +7,15 @@ var x = d3.scaleLinear()
 var chart = d3.select(".chart")
     .attr("width", width);
 
-d3.tsv("./data.tsv", type, function(error, data) {
+var data = [
+  {name: "Locke",    value:  4},
+  {name: "Reyes",    value:  8},
+  {name: "Ford",     value: 15},
+  {name: "Jarrah",   value: 16},
+  {name: "Shephard", value: 23},
+  {name: "Kwon",     value: 42}
+];
+
   x.domain([0, d3.max(data, function(d) { return d.value; })]);
 
   chart.attr("height", barHeight * data.length);
@@ -26,7 +34,7 @@ d3.tsv("./data.tsv", type, function(error, data) {
       .attr("y", barHeight / 2)
       .attr("dy", ".35em")
       .text(function(d) { return d.value; });
-});
+
 
 function type(d) {
   d.value = +d.value; // coerce to number
