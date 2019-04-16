2a3eb7688c523e39916ad076e6041ccf0881694e
Jason Lin <jasonlin43212@gmail.com>
2019-04-16 13:50:41 -0400

    hw updated

--- 18_listcomp/list.py
+++ 18_listcomp/list.py
@@ -2,13 +2,10 @@ import math
 # Pythagorean Triples
 
 def triple(n):
-    return set(sum([ [x,y,int(math.sqrt(math.pow(x,2)+math.pow(y,2)))]\
-                    if ((math.sqrt(math.pow(x,2)+math.pow(y,2))).is_integer() and math.sqrt(math.pow(x,2)+math.pow(y,2))<n)\
-                    else [x,y] if math.sqrt(math.pow(x,2)+math.pow(y,2)).is_integer()\
-                    else []\
-                    for x in range(1,n) for y in range(1,n)\
-                    ],[]))
-# print(triple(100))
+    return [{x,y,int(math.sqrt(math.pow(x,2)+math.pow(y,2)))}\
+             for x in range(1,n) for y in range(x,n)\
+             if ((math.sqrt(math.pow(x,2)+math.pow(y,2))).is_integer() and math.sqrt(math.pow(x,2)+math.pow(y,2))<n)]
+print(triple(100))
 
 def quicksort(arr): return sum([[] if len(arr) == 0 else quicksort([x for x in arr if x < arr[0]]) + [x for x in arr if x == arr[0]] + quicksort([x for x in arr if x > arr[0]])],[])
 
