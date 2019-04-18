842b55feb50fd7b672e895f9294e6ac1aedc9344
Jason Lin <jasonlin43212@gmail.com>
2019-04-18 02:23:49 -0400

    made it a function

--- 20_anon-reduce/book.py
+++ 20_anon-reduce/book.py
@@ -1,6 +1,6 @@
 # Team Reduce Reuse Recycle -- Simon Tsui and Jason Lin
 # SoftDev2 pd7
-# K20 --
+# K20 -- Reductio ad Absurdum
 # 2019-04-17
 
 from functools import reduce
@@ -21,7 +21,8 @@ def word_freq(word):
 
 # print(word_freq("entirely"))
 
-phrase = "in the".split()
-group = reduce((lambda x,y: x+y),[1 if words[i:i+len(phrase)] == phrase else 0 for i in range(len(words)-len(phrase)+1) ])
+def phrase_freq(phrase):
+    phrase = phrase.split()
+    return reduce((lambda x,y: x+y),[1 if words[i:i+len(phrase)] == phrase else 0 for i in range(len(words)-len(phrase)+1) ])
 # group = reduce((lamda x,y : [x[0]+1,x[1]+y] if y == phrase[x[0]] else [0,'']), words)
-# print(group)
+print(phrase_freq("over the"))
