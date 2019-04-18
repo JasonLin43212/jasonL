593554042e157f6aca7cb7c172f5badf9b24d32b
Jason Lin <jasonlin43212@gmail.com>
2019-04-18 10:15:18 -0400

    made actual function for most frequent but stuck on list comp and reduce

--- 20_anon-reduce/book.py
+++ 20_anon-reduce/book.py
@@ -9,7 +9,9 @@ import string
 f = open("tom.txt",'r')
 words = f.read()
 for c in string.punctuation:
-    words = words.replace(c,"")
+    words = words.replace(c," ")
+words = words.replace('“'," ")
+words = words.replace('”'," ")
 words = words.split()
 f.close()
 words = [0] + words
@@ -25,4 +27,23 @@ def phrase_freq(phrase):
     phrase = phrase.split()
     return reduce((lambda x,y: x+y),[1 if words[i:i+len(phrase)] == phrase else 0 for i in range(len(words)-len(phrase)+1) ])
 # group = reduce((lamda x,y : [x[0]+1,x[1]+y] if y == phrase[x[0]] else [0,'']), words)
-print(phrase_freq("over the"))
+# print(phrase_freq("over the"))
+
+def get_most_frequent():
+    word_dict = {}
+    for word in words[1:]:
+        word = word.lower()
+        if word in word_dict.keys():
+            word_dict[word] = word_dict[word] + 1
+        else:
+            word_dict[word] = 1
+    most_word = ''
+    high_freq = 0
+    for key in word_dict:
+        if word_dict[key] > high_freq:
+            most_word = key
+            high_freq = word_dict[key]
+    return most_word,high_freq
+    # return reduce((lambda x,y: ))
+    # return reduce((lambda x,y: [y,word_freq(y)] if word_freq(y) > x[1] else x),[["",0]] + list(set(words[1:])))
+print(get_most_frequent())
