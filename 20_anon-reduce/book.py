3993e76b1049162e1e13ece1f4c235d7805ecc99
Jason Lin <jasonlin43212@gmail.com>
2019-04-18 13:36:27 -0400

    finish

--- 20_anon-reduce/book.py
+++ 20_anon-reduce/book.py
@@ -26,25 +26,21 @@ def phrase_freq(phrase):
     return reduce((lambda x,y: x+y),[1 if words[i:i+len(phrase)] == phrase\
                                        else 0\
                                        for i in range(len(words)-len(phrase)+1)],0)
-print(phrase_freq("in the"))
+# print(phrase_freq("in the"))
 
-# Goes through the entire book once and counts the occurance of each word.
-# We were unable to find a reduce/listcomp way to do this
+def incr_dict(dict, word):
+    new_dict = dict.copy()
+    word = word.lower()
+    if word in new_dict.keys():
+        new_dict[word] = new_dict[word] + 1
+    else:
+        new_dict[word] = 1
+    return new_dict
+
+# Goes through the entire book once and counts the occurance of each word in a dictionary
+# Then find the highest frequency word
 def get_most_frequent():
-    word_dict = {}
-    for word in words[1:]:
-        word = word.lower()
-        if word in word_dict.keys():
-            word_dict[word] = word_dict[word] + 1
-        else:
-            word_dict[word] = 1
-    most_word = ''
-    high_freq = 0
-    for key in word_dict:
-        if word_dict[key] > high_freq:
-            most_word = key
-            high_freq = word_dict[key]
-    return most_word,high_freq
-    # return reduce((lambda x,y: ))
-    # return reduce((lambda x,y: [y,word_freq(y)] if word_freq(y) > x[1] else x),[["",0]] + list(set(words[1:])))
-# print(get_most_frequent())
+    all_word_freq = reduce((lambda x,y: incr_dict(x,y)), words, {"+":-1})
+    return reduce((lambda a,b: [b,all_word_freq[b]] if all_word_freq[b] > a[1] else a),all_word_freq.keys(),['',0])
+    
+print(get_most_frequent())
